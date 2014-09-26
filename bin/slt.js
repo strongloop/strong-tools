03fbc19b0b7a23215cd8853bbba8b259a5bf4d0e
Ryan Graham <ryan@strongloop.com>
2014-09-26 10:43:01 -0700

    Convert to sub-command model

---
+++ bin/slt.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var path = require('path');
+var tools = require('../');
+
+var cmd = process.argv[2] || 'help';
+var pkg = path.resolve(process.argv[3] || './package.json');
+
+if (cmd in tools) {
+  if (tools[cmd].cli)
+    tools[cmd].cli(pkg);
+  else
+    tools[cmd](pkg);
+} else {
+  usage(path.basename(process.argv[1]), console.log.bind(console));
+  process.exit(1);
+}
+
+function usage($0, p) {
+  p('Usage: %s <CMD> [PKG]', $0);
+  p('');
+  p('Commands:');
+  p('  lint      Perform a simple linting of the given package.json');
+  p('  cla       Create or verify contribution guidelines');
+  p('  help      Print this usage guide');
+  p('');
+}
