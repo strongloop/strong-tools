3e8aa1bbfc75ad9f576d65b0f9dea00554a5b491
Ryan Graham <ryan@strongloop.com>
2014-10-06 16:02:15 -0700

    Add project abstraction
    The Project encapsulates the package.json and metadata about it, such as
    file and directory paths.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
   lint: require('./lib/lint'),
   cla: require('./lib/cla'),
+  Project: require('./lib/project'),
 };
--- test/test-cla.js
+++ test/test-cla.js
@@ -2,6 +2,7 @@ var assert = require('tapsert');
 var fs = require('fs');
 var path = require('path');
 var rimraf = require('rimraf');
+var Project = require('../lib/project');
 
 var SANDBOX = path.resolve(__dirname, 'SANDBOX');
 var SANDBOX_PKG = path.resolve(SANDBOX, 'package.json');
@@ -11,6 +12,8 @@ var SANDBOX_CONTRIB_SUGG = path.resolve(SANDBOX, 'CONTRIBUTING.md.suggested');
 rimraf.sync(SANDBOX);
 fs.mkdirSync(SANDBOX);
 fs.writeFileSync(SANDBOX_PKG, JSON.stringify({name: 'testing'}), 'utf8');
+
+var sandbox_pkg = new Project(SANDBOX_PKG);
 
 var cla = require('../').cla;
 var output = [];
@@ -24,14 +27,14 @@ assert(!fs.existsSync(SANDBOX_CONTRIB),
 assert(!fs.existsSync(SANDBOX_CONTRIB_SUGG),
        'Starting with no CONTIRBUTING.md.suggested');
 
-assert.doesNotThrow(cla.bind(null, SANDBOX_PKG, logger),
+assert.doesNotThrow(cla.bind(null, sandbox_pkg, logger),
                     'Runs when only the package.json exists');
 assert(fs.existsSync(SANDBOX_CONTRIB),
        '  A default CONTRIBUTING.md is created');
 assert(!fs.existsSync(SANDBOX_CONTRIB_SUGG),
        '  A CONTRIBUTIONG.md.suggested is NOT created');
 
-assert.doesNotThrow(cla.bind(null, SANDBOX_PKG, logger),
+assert.doesNotThrow(cla.bind(null, sandbox_pkg, logger),
                     'Re-runs when a correct CONTRIBUTING.md exists');
 assert(fs.existsSync(SANDBOX_CONTRIB),
        '  The original CONTRIBUTING.md still exists');
@@ -39,7 +42,7 @@ assert(!fs.existsSync(SANDBOX_CONTRIB_SUGG),
        '  A CONTRIBUTIONG.md.suggested is NOT created');
 
 fs.writeFileSync(SANDBOX_CONTRIB, 'This is the stuff', 'utf8');
-assert.doesNotThrow(cla.bind(null, SANDBOX_PKG, logger),
+assert.doesNotThrow(cla.bind(null, sandbox_pkg, logger),
                     'Re-runs when an incorrect CONTRIBUTING.md exists');
 assert(fs.existsSync(SANDBOX_CONTRIB),
        '  The original CONTRIBUTING.md still exists');
