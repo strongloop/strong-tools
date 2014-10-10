b9733c681955478e12134fd8aede5f9ff5eb2843
Ryan Graham <ryan@strongloop.com>
2014-10-09 18:37:54 -0700

    Add slt-changelog command
    Hey, you got Ruby in my JavaScript\!

---
+++ bin/slt-changelog.rb
@@ -0,0 +1,104 @@
+#!/usr/bin/env ruby
+
+require 'pathname'
+
+class GitRepo
+  def initialize(repo='.')
+    @repo = repo
+    @sha1 = Hash.new do |cache, ref|
+      cache[ref] = `#{git} rev-list -n 1 #{ref}`.strip
+    end
+  end
+
+  def changelog(unreleased=false)
+    # --full-history: include individual commits from merged branches
+    # --date-order: order commits by date, not topological order
+    base = "#{git} log --full-history --date-order --pretty='format:%s (%an)'"
+    releases = []
+    tags = `#{git} tag`.strip.lines.map(&:strip)
+    return '' if tags.empty?
+    release_heading = `#{git} log --date=short --format="%ad" -n1 '#{tags.first}'`.strip + ", Version #{clean_version(tags.first)}"
+    release = []
+    release << " * First release!"
+    release << "#{release_heading}\n#{'='*release_heading.length}\n"
+    releases << release.reverse.join("\n")
+    tags.each_cons(2) do |a,b|
+      release_heading = `#{git} log --date=short --format="%ad" -n1 '#{b}'`.strip + ", Version #{clean_version(b)}"
+      release = []
+      release << changelog_filter(`#{base} '#{sha1(a)}..#{sha1(b)}'`)
+      next if release.empty?
+      release << "#{release_heading}\n#{'='*release_heading.length}\n"
+      releases << release.reverse.join("\n")
+    end
+    if unreleased
+      last_release = `#{git} rev-list --tags --max-count=1`.strip
+      if sha1('HEAD') != sha1(last_release) and not last_release.empty?
+        release_tag = `#{git} tag --points-at=#{last_release}`.strip
+        release_heading = `#{git} log --date=short --format="Changes since %ad" -n1 #{last_release}`.strip + ", Version #{clean_version(release_tag)}"
+        release = []
+        release << changelog_filter(`#{base} #{last_release}..`)
+        release << "#{release_heading}\n#{'='*release_heading.length}\n" unless release.empty?
+        releases << release.reverse.join("\n") unless release.empty?
+      end
+    end
+    releases.reverse.join("\n\n") + "\n"
+  end
+
+  def changelog_filter(log)
+    log.lines
+       .map(&:strip)
+       .reject { |line| line =~ /^Merge/ }
+       .reject { |line| line =~ /^v?\d+\.\d+\.\d+ \(/ }
+       .reject { |line| line =~ /update changes.md/i }
+       .reject { |line| line =~ /update changelog/i }
+       .to_a.uniq
+       .map { |line| " * #{line}\n" }
+       .join("\n")
+  end
+
+  def sha1(ref)
+    if ref.empty? or ref.nil?
+      nil
+    else
+      @sha1[ref]
+    end
+  end
+
+  def clean_version(tag)
+    tag.gsub(/^v/, '')
+  end
+
+  private
+  attr_reader :repo
+
+  def git
+    "git --git-dir=#{ensure_repo}"
+  end
+
+  def ensure_repo
+    @dir ||= begin
+      if system "git rev-parse --show-toplevel"
+        `git rev-parse --show-toplevel`.strip + '/.git'
+      else
+        dir = Dir.mktmpdir
+        system "git clone --bare #{repo} #{dir}"
+        Pathname(dir)
+      end
+    end
+  end
+
+end
+
+repo = GitRepo.new('.')
+show_unreleased = false
+filename = 'CHANGES.md'
+ARGV.each do |arg|
+  case arg
+  when '-u', '--unreleased'
+    show_unreleased = true
+  else
+    filename = arg
+  end
+end
+changelog = repo.changelog(show_unreleased)
+IO.write(filename, changelog)
