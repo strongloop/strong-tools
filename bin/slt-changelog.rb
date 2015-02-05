eed0551ba97f98cd38eab84473063426a204dd9c
Ryan Graham <ryan@strongloop.com>
2015-02-04 16:38:23 -0800

    slt-changelog: refactor internals
    Many small changes have been made over time while the core logic has
    actually been simplified. Now that simplicity is far more obvious.

--- bin/slt-changelog.rb
+++ bin/slt-changelog.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'date'
 require 'optparse'
 require 'pathname'
 
@@ -9,6 +10,9 @@ class GitRepo
     @sha1 = Hash.new do |cache, ref|
       cache[ref] = `#{git} rev-list -n 1 #{ref}`.strip
     end
+    @date_of = Hash.new do |cache, ref|
+      cache[ref] = DateTime.parse(`#{git} log --date=iso-strict --format="%ad" -n1 '#{ref}'`.strip) #.utc
+    end
   end
 
   def changelog(next_release=false)
@@ -17,30 +21,35 @@ class GitRepo
     base = "#{git} log --full-history --date-order --pretty='format:%s (%an)'"
     releases = []
     tags = tags_by_topo
-    return '' if tags.empty?
-    release_heading = `#{git} log --date=short --format="%ad" -n1 '#{tags.first}'`.strip + ", Version #{clean_version(tags.first)}"
+
+    # First release doesn't have a full changelog
+    if tags.empty?
+      release_heading = "#{Time.now.utc.strftime("%Y-%m-%d")}, Version #{clean_version(next_release || '0.0.0')}"
+    else
+      release_heading = "#{date_of(tags.first).strftime("%Y-%m-%d")}, Version #{clean_version(tags.first)}"
+    end
     release = []
     release << " * First release!"
     release << "#{release_heading}\n#{'='*release_heading.length}\n"
     releases << release.reverse.join("\n")
+
+    # 2nd, 3rd, etc. releases have changes between them
     tags.each_cons(2) do |a,b|
-      release_heading = `#{git} log --date=short --format="%ad" -n1 '#{b}'`.strip + ", Version #{clean_version(b)}"
+      release_heading = "#{date_of(b).strftime("%Y-%m-%d")}, Version #{clean_version(b)}"
       release = []
       release << changelog_filter(`#{base} '#{sha1(a)}..#{sha1(b)}'`)
       next if release.empty?
       release << "#{release_heading}\n#{'='*release_heading.length}\n"
       releases << release.reverse.join("\n")
     end
-    if next_release
-      last_release = `#{git} rev-list --tags --max-count=1`.strip
-      if sha1('HEAD') != sha1(last_release) and not last_release.empty?
-        release_tag = `#{git} tag --points-at=#{last_release}`.strip
-        release_heading = "#{Time.now.utc.strftime("%Y-%m-%d")}, Version #{clean_version(next_release)}"
-        release = []
-        release << changelog_filter(`#{base} #{last_release}..`)
-        release << "#{release_heading}\n#{'='*release_heading.length}\n" unless release.empty?
-        releases << release.reverse.join("\n") unless release.empty?
-      end
+
+    # Release we are pretending HEAD is
+    if next_release and tags.length > 0 and sha1('HEAD') != sha1(tags.last)
+      release_heading = "#{Time.now.utc.strftime("%Y-%m-%d")}, Version #{clean_version(next_release)}"
+      release = []
+      release << changelog_filter(`#{base} #{sha1(tags.last)}..`)
+      release << "#{release_heading}\n#{'='*release_heading.length}\n" unless release.empty?
+      releases << release.reverse.join("\n") unless release.empty?
     end
     releases.reverse.join("\n\n") + "\n"
   end
@@ -94,6 +103,14 @@ class GitRepo
       nil
     else
       @sha1[ref]
+    end
+  end
+
+  def date_of(ref)
+    if ref.nil? or ref.empty?
+      nil
+    else
+      @date_of[sha1(ref)]
     end
   end
 
