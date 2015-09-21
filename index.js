b3a1d0109ccf90981c166a3063ddb9510822cab9
Ryan Graham <ryan@strongloop.com>
2015-09-21 10:23:13 -0700

    fix crash on node-v0.10
    All of our CI bots that stage packages use node-v0.10, but don't make
    use of the license command, so make that command only available when it
    is supported.

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var wrapped = require('./lib/wrapped');
 
 module.exports = {
   lint: require('./lib/lint'),
-  'license': require('./lib/license'),
   cla: require('./lib/cla'),
   Project: require('./lib/project'),
   info: require('./lib/info'),
   version: require('./lib/version'),
   semver: wrapped('semver/bin/semver'),
 };
+
+if (require('child_process').execSync) {
+  module.exports.license =  require('./lib/license');
+}
