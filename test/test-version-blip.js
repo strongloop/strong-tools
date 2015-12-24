d95d955af85afc49c3da9c3b70c50a16765021dc
Ryan Graham <ryan@strongloop.com>
2015-12-24 15:30:20 -0800

    replace blip dep with blip script

--- test/test-version-blip.js
+++ test/test-version-blip.js
@@ -33,6 +33,7 @@ assert(original.optionalDependencies,
        'sl-blip dependency updates when present');
 assert.strictEqual(original.optionalDependencies['sl-blip'], '*',
                    '-- initial version');
+assert(!('scripts' in original), 'no scripts set initially');
 
 var newVer = false;
 tools.version.cli.out = function(output) {
@@ -43,6 +44,36 @@ assert.strictEqual(newVer, 'testing@1.2.3',
                    '-- prints name@version');
 
 updated = JSON.parse(fs.readFileSync(SANDBOX_PKG, 'utf8'));
-assert.strictEqual(updated.optionalDependencies['sl-blip'],
-                   'http://blip.strongloop.com/testing@1.2.3',
-                   '-- sl-blip "version" updated');
+assert(!('sl-blip' in updated.optionalDependencies), '-- sl-blip removed');
+assert.strictEqual(updated.scripts.preinstall, 'node scripts/sl-blip.js',
+                   '-- injects sl-blip as a preinstall script');
+
+var withBlipAndPreinstall = {
+  name: 'testing',
+  scripts: {
+    preinstall: 'something other than blip',
+  },
+  optionalDependencies: {
+    'sl-blip': '*',
+  },
+};
+fs.writeFileSync(SANDBOX_PKG, JSON.stringify(withBlipAndPreinstall), 'utf8');
+original = JSON.parse(fs.readFileSync(SANDBOX_PKG, 'utf8'));
+assert(original.optionalDependencies,
+      'sl-blip dependency updates when present');
+assert.strictEqual(original.optionalDependencies['sl-blip'], '*',
+                  '-- initial version');
+assert.strictEqual(original.scripts.preinstall, 'something other than blip',
+                   '-- initial preinstall script');
+
+newVer = false;
+tools.version.cli('set', '1.2.3', SANDBOX_PKG);
+assert.strictEqual(newVer, 'testing@1.2.3',
+                   '-- prints name@version');
+
+updated = JSON.parse(fs.readFileSync(SANDBOX_PKG, 'utf8'));
+assert(!('sl-blip' in updated.optionalDependencies), '-- sl-blip removed');
+assert.strictEqual(updated.scripts.preinstall, original.scripts.preinstall,
+                   '-- original preinstall script preserved');
+assert.strictEqual(updated.scripts.postinstall, 'node .sl-blip.js',
+                   '-- injects as postinstall script');
