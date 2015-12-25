4c45850905ac3d5ae1ed0bd5180ad112dadb563d
Ryan Graham <ryan@strongloop.com>
2015-12-24 16:00:08 -0800

    update blip script with package persistence

--- test/test-version-blip.js
+++ test/test-version-blip.js
@@ -4,7 +4,17 @@ var path = require('path');
 var rimraf = require('rimraf');
 var tools = require('../');
 
+function exists(path) {
+  if (fs.accessSync) {
+    try { fs.accessSync(path); } catch (e) { return false; }
+    return true;
+  }
+  return fs.existsSync(path);
+}
+
+var BLIP_SRC = fs.readFileSync(require.resolve('../lib/blip'), 'utf8');
 var SANDBOX = path.resolve(__dirname, 'SANDBOX-blip');
+var SANDBOX_BLIP = path.resolve(SANDBOX, '.sl-blip.js');
 var SANDBOX_PKG = path.resolve(SANDBOX, 'package.json');
 
 rimraf.sync(SANDBOX);
@@ -34,6 +44,7 @@ assert(original.optionalDependencies,
 assert.strictEqual(original.optionalDependencies['sl-blip'], '*',
                    '-- initial version');
 assert(!('scripts' in original), 'no scripts set initially');
+assert(!exists(SANDBOX_BLIP), '-- no file at .sl-blip.js');
 
 var newVer = false;
 tools.version.cli.out = function(output) {
@@ -45,8 +56,11 @@ assert.strictEqual(newVer, 'testing@1.2.3',
 
 updated = JSON.parse(fs.readFileSync(SANDBOX_PKG, 'utf8'));
 assert(!('sl-blip' in updated.optionalDependencies), '-- sl-blip removed');
-assert.strictEqual(updated.scripts.preinstall, 'node scripts/sl-blip.js',
+assert.strictEqual(updated.scripts.preinstall, 'node .sl-blip.js',
                    '-- injects sl-blip as a preinstall script');
+assert(exists(SANDBOX_BLIP), '-- .sl-blip.js was created');
+assert.strictEqual(fs.readFileSync(SANDBOX_BLIP, 'utf8'), BLIP_SRC,
+                   '-- blip script content is correct');
 
 var withBlipAndPreinstall = {
   name: 'testing',
@@ -57,6 +71,7 @@ var withBlipAndPreinstall = {
     'sl-blip': '*',
   },
 };
+fs.writeFileSync(SANDBOX_BLIP, 'something else', 'utf8');
 fs.writeFileSync(SANDBOX_PKG, JSON.stringify(withBlipAndPreinstall), 'utf8');
 original = JSON.parse(fs.readFileSync(SANDBOX_PKG, 'utf8'));
 assert(original.optionalDependencies,
@@ -65,6 +80,8 @@ assert.strictEqual(original.optionalDependencies['sl-blip'], '*',
                   '-- initial version');
 assert.strictEqual(original.scripts.preinstall, 'something other than blip',
                    '-- initial preinstall script');
+assert.strictEqual(fs.readFileSync(SANDBOX_BLIP, 'utf8'), 'something else',
+                   '-- blip script content is garbage');
 
 newVer = false;
 tools.version.cli('set', '1.2.3', SANDBOX_PKG);
@@ -77,3 +94,5 @@ assert.strictEqual(updated.scripts.preinstall, original.scripts.preinstall,
                    '-- original preinstall script preserved');
 assert.strictEqual(updated.scripts.postinstall, 'node .sl-blip.js',
                    '-- injects as postinstall script');
+assert.strictEqual(fs.readFileSync(SANDBOX_BLIP, 'utf8'), BLIP_SRC,
+                   '-- blip script content is replaced');
