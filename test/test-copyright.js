82276d26ca7f4984636d6cee289e46124292823c
Ryan Graham <r.m.graham@gmail.com>
2016-03-07 12:15:52 -0800

    basic copyright header generator functions

--- test/test-copyright.js
+++ test/test-copyright.js
@@ -18,3 +18,36 @@ test('copyright years', function(t) {
   });
   t.end();
 });
+
+test('copyright headers', function(t) {
+  t.test('MIT license', function(t) {
+    return copyright.header(__filename, 'MIT').then(function(header) {
+      testCopyrightStatement(t, header);
+      t.match(header, 'MIT License');
+      t.match(header, 'at http://some-url/');
+    });
+  });
+  t.test('Artistic license', function(t) {
+    return copyright.header(__filename, 'Artistic').then(function(header) {
+      testCopyrightStatement(t, header);
+      t.match(header, 'Artistic License 2.0');
+      t.match(header, 'in LICENSE.md');
+    });
+  });
+  t.test('Commercial license', function(t) {
+    return copyright.header(__filename, 'commercial').then(function(header) {
+      testCopyrightStatement(t, header);
+      t.notMatch(header, 'Artistic');
+      t.notMatch(header, 'MIT');
+      t.match(header, 'US Government Users Restricted Rights');
+      t.match(header, 'Use, duplication or disclosure');
+      t.match(header, 'restricted by GSA ADP Schedule Contract with IBM Corp.');
+    });
+  });
+  t.end();
+});
+
+function testCopyrightStatement(t, str) {
+  t.match(str, /Licensed Materials - Property of IBM$/m);
+  t.match(str, /Copyright IBM Corp\. \d{4}(,\d{4})*\. All Rights Reserved\.$/m);
+}
