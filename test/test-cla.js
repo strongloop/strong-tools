b1f33627542af8fdef08d6318776d2b92e8dff43
Ryan Graham <r.m.graham@gmail.com>
2016-05-25 19:02:41 -0700

    cla: refactor to easier async testing

--- test/test-cla.js
+++ test/test-cla.js
@@ -19,44 +19,49 @@ var SANDBOX = path.resolve(__dirname, 'SANDBOX-cla');
 var SANDBOX_PKG = path.resolve(SANDBOX, 'package.json');
 var SANDBOX_CONTRIB = path.resolve(SANDBOX, 'CONTRIBUTING.md');
 var SANDBOX_CONTRIB_SUGG = path.resolve(SANDBOX, 'CONTRIBUTING.md.suggested');
+var sandbox_pkg = null;
 
 test('setup', function(t) {
   rimraf.sync(SANDBOX);
   fs.mkdirSync(SANDBOX);
   fs.writeFileSync(SANDBOX_PKG, JSON.stringify({name: 'testing'}), 'utf8');
-  t.pass('setup complete');
+  sandbox_pkg = new Project(SANDBOX_PKG);
+  t.assert(cla, 'cla is exported');
+  t.ok(sandbox_pkg, 'sandbox created');
+  t.ok(!fs.existsSync(SANDBOX_CONTRIB),
+       'Starting with no CONTIRBUTING.md');
+  t.ok(!fs.existsSync(SANDBOX_CONTRIB_SUGG),
+       'Starting with no CONTIRBUTING.md.suggested');
   t.end();
 });
 
-test('CLA generation', function(t) {
-  var sandbox_pkg = new Project(SANDBOX_PKG);
+test('No CONTRIBUTING.md files exist', function(t) {
+  cla(sandbox_pkg, logger, function() {
+    t.ok(fs.existsSync(SANDBOX_CONTRIB),
+         'A default CONTRIBUTING.md is created');
+    t.ok(!fs.existsSync(SANDBOX_CONTRIB_SUGG),
+         'A CONTRIBUTIONG.md.suggested is NOT created');
+    t.end();
+  });
+});
 
-  t.assert(cla, 'cla is exported');
-  t.assert(!fs.existsSync(SANDBOX_CONTRIB),
-         'Starting with no CONTIRBUTING.md');
-  t.assert(!fs.existsSync(SANDBOX_CONTRIB_SUGG),
-         'Starting with no CONTIRBUTING.md.suggested');
+test('CONTRIBUTING.md already up to date', function(t) {
+  cla(sandbox_pkg, logger, function() {
+    t.ok(fs.existsSync(SANDBOX_CONTRIB),
+         'The original CONTRIBUTING.md still exists');
+    t.ok(!fs.existsSync(SANDBOX_CONTRIB_SUGG),
+         'A CONTRIBUTIONG.md.suggested is NOT created');
+    t.end();
+  });
+});
 
-  t.doesNotThrow(cla.bind(null, sandbox_pkg, logger),
-                      'Runs when only the package.json exists');
-  t.assert(fs.existsSync(SANDBOX_CONTRIB),
-         '  A default CONTRIBUTING.md is created');
-  t.assert(!fs.existsSync(SANDBOX_CONTRIB_SUGG),
-         '  A CONTRIBUTIONG.md.suggested is NOT created');
-
-  t.doesNotThrow(cla.bind(null, sandbox_pkg, logger),
-                      'Re-runs when a correct CONTRIBUTING.md exists');
-  t.assert(fs.existsSync(SANDBOX_CONTRIB),
-         '  The original CONTRIBUTING.md still exists');
-  t.assert(!fs.existsSync(SANDBOX_CONTRIB_SUGG),
-         '  A CONTRIBUTIONG.md.suggested is NOT created');
-
+test('CONTRIBUTING.md exists, but is wrong', function(t) {
   fs.writeFileSync(SANDBOX_CONTRIB, 'This is the stuff', 'utf8');
-  t.doesNotThrow(cla.bind(null, sandbox_pkg, logger),
-                      'Re-runs when an incorrect CONTRIBUTING.md exists');
-  t.assert(fs.existsSync(SANDBOX_CONTRIB),
-         '  The original CONTRIBUTING.md still exists');
-  t.assert(fs.existsSync(SANDBOX_CONTRIB_SUGG),
-         '  A CONTRIBUTIONG.md.suggested is created');
-  t.end();
+  cla(sandbox_pkg, logger, function() {
+    t.ok(fs.existsSync(SANDBOX_CONTRIB),
+         'The original CONTRIBUTING.md still exists');
+    t.ok(fs.existsSync(SANDBOX_CONTRIB_SUGG),
+         'A CONTRIBUTIONG.md.suggested is created');
+    t.end();
+  });
 });
